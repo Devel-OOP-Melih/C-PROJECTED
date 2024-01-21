b05c3d62c8c2da3929ced88a314644084eefa9ad
Devel-OOP-Melih <157290862+Devel-OOP-Melih@users.noreply.github.com>
2024-01-21 16:32:17 +0300

    Inheritance, Object References & Composition

---
+++ ElektrikliUretim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_References
+{
+    internal class ElektrikliUretim : OtomobilUretim
+    {
+        private int sarjDurumu;
+
+        public ElektrikliUretim(int sarjDurumu, string renk, string jant)
+        {
+            this.sarjDurumu = sarjDurumu;
+            this.RENK = renk;
+            this.JANT = jant;
+        }
+
+        public string sarjDurumuGoster()
+        {
+            return sarjDurumu + " kWh.";
+        }
+
+        public void sarjEt()
+        {
+            this.sarjDurumu = 250;
+        }
+    }
+}
